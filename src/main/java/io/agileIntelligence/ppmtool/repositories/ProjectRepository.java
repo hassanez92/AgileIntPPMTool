2ce967b32520f6d3f54ab998e579b43a2768131b
hassanez92 <hassanez92@gmail.com>
2021-12-02 22:03:30 -0600

    branch 8

--- src/main/java/io/agileIntelligence/ppmtool/repositories/ProjectRepository.java
+++ src/main/java/io/agileIntelligence/ppmtool/repositories/ProjectRepository.java
@@ -1,6 +1,7 @@
 package io.agileIntelligence.ppmtool.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+
 import org.springframework.stereotype.Repository;
 
 import io.agileIntelligence.ppmtool.domain.Project;
@@ -11,5 +12,5 @@ public interface ProjectRepository extends CrudRepository<Project, Long> {
 	@Override
 	Iterable<Project> findAllById(Iterable<Long> iterable);
 	
-	
+	Project findByProjectIdentifier(String projectId);
 }
--- src/main/java/io/agileIntelligence/ppmtool/services/ProjectService.java
+++ src/main/java/io/agileIntelligence/ppmtool/services/ProjectService.java
@@ -25,4 +25,14 @@ public class ProjectService {
 		
 		
 	}
+	public Project findProjectByIdentifier(String projectId) {
+		
+		Project project =projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		 
+		if(project == null) {
+			throw new ProjectIdException("Project ID'"+projectId+  "' doesn't exists");
+		}
+		
+		return project;
+	}
 }
