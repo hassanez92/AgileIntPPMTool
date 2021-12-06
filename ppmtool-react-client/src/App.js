9423d5b8aa8a3f6c6d6abb55bbda47d878ea85e8
hassanez92 <hassanez92@gmail.com>
2021-12-06 13:05:15 -0600

    branch 15

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import { Component } from 'react';
 import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render(){
   return (
     <div className="App">
+      <Header />
       <Dashboard />
     </div>
-  );
+    );
   }
 }
 
