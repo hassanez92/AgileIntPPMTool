01648e6158a0763d7e8b765da80d29178e9ea436
hassanez92 <hassanez92@gmail.com>
2021-12-08 18:31:52 -0600

    branch 18

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,19 +3,23 @@ import React, { Component } from "react";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
-  return (
-    <Router>
-    <div className="App dark-overlay">
-      <Header />
-      <Route exact path="/dashboard" component={Dashboard} />
-      <Route exact path="/addProject" component={AddProject} />
-    </div>
-    </Router>
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App dark-overlay">
+            <Header />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
