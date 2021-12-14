a8a191c7918f776abb3cc83f795d96640a175f54
hassanez92 <hassanez92@gmail.com>
2021-12-14 12:18:55 -0600

    branch 31

---
+++ src/main/java/io/agileIntelligence/ppmtool/repositories/BackLogRepository.java
@@ -0,0 +1,11 @@
+package io.agileIntelligence.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import io.agileIntelligence.ppmtool.domain.Backlog;
+
+@Repository
+public interface BackLogRepository extends CrudRepository<Backlog, Long>{
+
+}
