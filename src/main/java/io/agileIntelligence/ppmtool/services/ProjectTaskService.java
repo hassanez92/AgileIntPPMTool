45b78a555ecd38f98a21fba51c035846a793a7d9
hassanez92 <hassanez92@gmail.com>
2021-12-15 10:16:08 -0600

    branch 35

--- src/main/java/io/agileIntelligence/ppmtool/services/ProjectTaskService.java
+++ src/main/java/io/agileIntelligence/ppmtool/services/ProjectTaskService.java
@@ -10,37 +10,40 @@ import io.agileIntelligence.ppmtool.repositories.ProjectTaskRepository;
 
 @Service
 public class ProjectTaskService {
-	
+
 	@Autowired
 	private BackLogRepository backlogRepository;
-	
+
 	@Autowired
 	private ProjectTaskRepository projectTaskRepository;
-	
+
 	public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
-		
-		//PTs to be added to a specific project !=null, BL exists
+
+		// PTs to be added to a specific project !=null, BL exists
 		Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-		//set backlog to pt
+		// set backlog to pt
 		projectTask.setBacklog(backlog);
-		//we want out project sequence to be like this: IDPRO-1 IDPRO-2
+		// we want out project sequence to be like this: IDPRO-1 IDPRO-2
 		Integer BacklogSequence = backlog.getPTSequence();
-		//Update the BL SEQUENCE
+		// Update the BL SEQUENCE
 		BacklogSequence++;
 		
-		//Add Sequence to Project Task
-		projectTask.setProjectSequence(projectIdentifier+"-"+BacklogSequence);
+		backlog.setPTSequence(BacklogSequence);
+
+		// Add Sequence to Project Task
+		projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
 		projectTask.setProjectIdentifier(projectIdentifier);
-		
-		//INITIAL priority when priority null
-	//	if(projectTask.getPriority()==0||projectTask.getPriority()==null) {
-		//	projectTask.setPriority(3);}
-		
-		//INITIAL status when status is null
-		if(projectTask.getStatus()==""||projectTask.getStatus()==null) {
+
+		// INITIAL priority when priority null
+		if (projectTask.getPriority() == null) { //In the future we need projectTAsk.getPriority() 
+			projectTask.setPriority(3);
+		}
+
+		// INITIAL status when status is null
+		if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
 			projectTask.setStatus("TO_DO");
 		}
-	
+
 		return projectTaskRepository.save(projectTask);
 	}
 
